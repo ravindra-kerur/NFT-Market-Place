3d86db5e8efc1218442e90bfcb6d2e013a4cee13
ravindrapatil <ravindra.spatil@gmail.com>
2023-12-20 08:52:16 +0530

    Footer changes

--- app/components/componentindex.js
+++ app/components/componentindex.js
@@ -2,5 +2,6 @@ import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navbar";
 import HeroSection from './HeroSection/HeroSection';
+import Service from './Service/Service';
 
-export { Button, Footer, Navbar, HeroSection };
+export { Button, Footer, Navbar, HeroSection, Service };
--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
 import Style from "./index.module.css";
-import { HeroSection } from "./components/componentindex";
+import { HeroSection, Service } from "./components/componentindex";
 
 export default function Home() {
   return (
     <div className={Style.homePage}>
       <HeroSection />
+      <Service />
     </div>
   );
 }
